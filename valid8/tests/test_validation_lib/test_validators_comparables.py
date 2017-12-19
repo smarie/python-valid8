61f83339d738649c7a6745d297ddc66557f7a70b
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-12-19 13:25:36 +0100

    Improved slightly the tests for comparables and numbers

--- valid8/tests/test_validation_lib/test_validators_comparables.py
+++ valid8/tests/test_validation_lib/test_validators_comparables.py
@@ -1,18 +1,18 @@
 import pytest
 
-from valid8 import gt, gts, lt, lts, between, Failure
+from valid8 import gt, gts, lt, lts, between, NotInRange, TooSmall, TooBig
 
 
 def test_gt():
     """ tests that the gt() function works """
     assert gt(1)(1)
-    with pytest.raises(Failure):
+    with pytest.raises(TooSmall):
         gt(-1)(-1.1)
 
 
 def test_gts():
     """ tests that the gts() function works """
-    with pytest.raises(Failure):
+    with pytest.raises(TooSmall):
         gts(1)(1)
     assert gts(-1)(-0.9)
 
@@ -20,13 +20,13 @@ def test_gts():
 def test_lt():
     """ tests that the lt() function works """
     assert lt(1)(1)
-    with pytest.raises(Failure):
+    with pytest.raises(TooBig):
         lt(-1)(-0.9)
 
 
 def test_lts():
     """ tests that the lts() function works """
-    with pytest.raises(Failure):
+    with pytest.raises(TooBig):
         lts(1)(1)
     assert lts(-1)(-1.1)
 
@@ -36,8 +36,8 @@ def test_between():
     assert between(0, 1)(0)
     assert between(0, 1)(1)
 
-    with pytest.raises(Failure):
+    with pytest.raises(NotInRange):
         between(0, 1)(-0.1)
 
-    with pytest.raises(Failure):
+    with pytest.raises(NotInRange):
         between(0, 1)(1.1)
--- valid8/tests/test_validation_lib/test_validators_numbers.py
+++ valid8/tests/test_validation_lib/test_validators_numbers.py
@@ -1,21 +1,21 @@
 import pytest
 
-from valid8 import is_even, is_odd, is_multiple_of, Failure
+from valid8 import is_even, is_odd, is_multiple_of, IsNotMultipleOf, IsNotOdd, IsNotEven
 
 
 def test_is_even():
     assert is_even(2)
-    with pytest.raises(Failure):
+    with pytest.raises(IsNotEven):
         is_even(-1)
 
 
 def test_is_odd():
     assert is_odd(-1)
-    with pytest.raises(Failure):
+    with pytest.raises(IsNotOdd):
         is_odd(-2)
 
 
 def test_is_multiple_of():
     assert is_multiple_of(3)(-9)
-    with pytest.raises(Failure):
+    with pytest.raises(IsNotMultipleOf):
         is_multiple_of(3)(-10)
