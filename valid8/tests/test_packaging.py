3000515afd2bc1761e91bcfa35a67fc4305cf513
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-03-07 15:26:03 +0100

    Added packaging tests

---
+++ valid8/tests/test_packaging.py
@@ -0,0 +1,8 @@
+
+def test_named_import():
+    import valid8 as v
+    o = v.is_in
+
+
+def test_import_from():
+    from valid8 import is_in
---
+++ valid8/tests/test_packaging_wild.py
@@ -0,0 +1,5 @@
+from valid8 import *  # this relies on '__all__'
+
+
+def test_wild_import():
+    o = is_in
